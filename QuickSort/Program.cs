f458845d65df655abf3a28c30ee3dc74ef8d1629
Nilufar Makhmudova <nmakhmudova@wiut.uz>
2016-07-02 12:26:18 +0500

    improved a little bit

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -11,19 +11,24 @@ namespace QuickSort
     {
         static void Main(string[] args)
         {
-            int[] _10integers = QuickSortHelper.ReadFiles(@"D:\Video\Stanford Algorithms\AlgoW2\10.txt");
+            int[] _10integers = QuickSortHelper.ReadFiles(@"D:\Video\Stanford Algorithms\AlgoW2\QuickSort\10.txt");
             QuickSort sort = new QuickSort();
             //fix running totals
+            SortResult result = sort.QuickSortAlgorithmWithFirstElementAsPivot(new SortResult() { Array = _10integers, numberOfComparisons = 0 });
+            Console.WriteLine(result.numberOfComparisons);
+            Console.WriteLine(result.Array.ToString());
+            Console.ReadKey();
         }
 
 
     }
 
     public  class QuickSort {
-        public int[] QuickSortAlgorithmWithFirstElementAsPivot(int[] integerArray) {
-            if (integerArray.Length == 1) return integerArray;
+        public SortResult QuickSortAlgorithmWithFirstElementAsPivot(SortResult integerArray) {
+            if (integerArray.Array.Length == 0) return new SortResult() { Array = new int[0], numberOfComparisons = integerArray.numberOfComparisons };
+            else if (integerArray.Array.Length == 1) return new SortResult() { Array = integerArray.Array, numberOfComparisons = integerArray.numberOfComparisons };
             else {
-                int pivot = integerArray[0];
+                int pivot = integerArray.Array[0];
                 PartitionResult partitionResult = DoPartition(integerArray, pivot);
                 QuickSortAlgorithmWithFirstElementAsPivot(partitionResult.FirstPart);
                 QuickSortAlgorithmWithFirstElementAsPivot(partitionResult.SecondPart);
@@ -31,19 +36,19 @@ namespace QuickSort
             }
         }
 
-        private PartitionResult DoPartition(int[] integerArray, int pivot)
+        private PartitionResult DoPartition(SortResult integerArray, int pivot)
         {
-            int numbOfComparisons = integerArray.Length-1;
+            int numbOfComparisons = integerArray.Array.Length-1 + integerArray.numberOfComparisons;
             //last index of integers that are less than pivot
-            int j = 0;
+            int j = 1;
 
-            for (int i =1; i<integerArray.Length;) {
-                if (integerArray[i] < pivot)
+            for (int i =1; i<integerArray.Array.Length;) {
+                if (integerArray.Array[i] < pivot)
                 {
                     //swap with unsortedArray[i] j+1
-                    int takeMeInt = integerArray[j];
-                    integerArray[j] = integerArray[i];
-                    integerArray[i] = takeMeInt;
+                    int takeMeInt = integerArray.Array[j];
+                    integerArray.Array[j] = integerArray.Array[i];
+                    integerArray.Array[i] = takeMeInt;
                     j++;
                     i++;
                 }
@@ -52,11 +57,11 @@ namespace QuickSort
                 }
             }
             //swap pivot with last in less part
-            int toComeFirst = integerArray[j];
-            integerArray[0] = toComeFirst;
-            integerArray[j] = pivot;
+            int toComeFirst = integerArray.Array[j-1];
+            integerArray.Array[0] = toComeFirst;
+            integerArray.Array[j-1] = pivot;
 
-            PartitionResult result = new PartitionResult() { FirstPart = integerArray.Take(j).ToArray(), SecondPart = integerArray.Skip(j + 1).ToArray(), numberOfComparisons= numbOfComparisons};
+            PartitionResult result = new PartitionResult() { FirstPart = new SortResult() { Array = integerArray.Array.Take(j-1).ToArray(), numberOfComparisons = numbOfComparisons }, SecondPart = new SortResult() { Array = integerArray.Array.Skip(j ).ToArray(), numberOfComparisons = numbOfComparisons } };
             return result;
         }
     }
@@ -76,8 +81,13 @@ namespace QuickSort
     }
 
     public class PartitionResult {
-        public int[] FirstPart { get; set; }
-        public int[] SecondPart { get; set; }
+        public SortResult FirstPart { get; set; }
+        public SortResult SecondPart { get; set; }
+   
+    }
+
+    public class SortResult {
+        public int[] Array { get; set; }
         public int numberOfComparisons { get; set; }
     }
 
--- QuickSortTests/10Tests.cs
+++ QuickSortTests/10Tests.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuickSort;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace QuickSortTests
 {
-    class _10Tests
+    [TestClass]
+   public  class _10Tests
     {
+        [TestMethod]
+        public void TestQuickSortFirstElementAsPivot() {
+            int expected = 25;
+            QuickSort.QuickSort sort = new QuickSort.QuickSort();
+            int[] arrayToBeSorted = QuickSort.QuickSortHelper.ReadFiles(@"D:\Video\Stanford Algorithms\AlgoW2\QuickSort\10.txt");
+            QuickSort.SortResult result = sort.QuickSortAlgorithmWithFirstElementAsPivot(new SortResult() { Array = arrayToBeSorted, numberOfComparisons = 0 });
+            int actual = result.numberOfComparisons;
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
